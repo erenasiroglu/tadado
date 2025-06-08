5fc43c3706ee7f8d058c730aed3cb4b95c86b73e
Eren Nasıroğlu <erenasiroglu1@gmail.com>
2025-06-08 16:29:21 +0300

    chore: add Supabase client and update dependencies

---
+++ lib/supabase.ts
@@ -0,0 +1,7 @@
+import { createClient } from '@supabase/supabase-js';
+import 'react-native-url-polyfill/auto';
+
+const supabaseUrl = 'https://fmfsknxseohmwvahcxgj.supabase.co';
+const supabaseAnonKey = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImZtZnNrbnhzZW9obXd2YWhjeGdqIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NDkzNzkxNjYsImV4cCI6MjA2NDk1NTE2Nn0.VBiqgR9xc2qxV-w6H6QJkpsFWEb-9kniw_93a84yNMk';
+
+export const supabase = createClient(supabaseUrl, supabaseAnonKey); 
